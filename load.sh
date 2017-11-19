213c821501891eed32eb2b13a4cc64d32a75608a
Johannes Hoffmann <johannesdotmdothoffmannatgmxdotde>
2017-11-19 13:35:03 +0100

    second

--- load.sh
+++ load.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 
+git clone https://github.com/j-m-hoffmann/dotfiles.git ~/dotfiles
 cp ~/dotfiles/bash_aliases ~/.bash_aliases
 cp ~/dotfiles/init.vim ~/.config/nvim/init.vim
--- save.sh
+++ save.sh
@@ -2,3 +2,4 @@
 
 cp ~/.bash_aliases ~/dotfiles/bash_aliases
 cp ~/.config/nvim/init.vim ~/dotfiles/init.vim
+git push -u origin master
